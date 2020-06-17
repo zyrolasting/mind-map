f3087c95a12444ddf4348070ff789a2e0d4c8409
Sage Gerard <sage@sagegerard.com>
2020-06-17 13:23:31 -0400

    Add description

--- info.rkt
+++ info.rkt
@@ -3,6 +3,6 @@
 (define deps '("base" "graphviz"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/mind-map.scrbl" ())))
-(define pkg-desc "Description Here")
+(define pkg-desc "Write and render mind maps in Racket")
 (define version "0.0")
 (define pkg-authors '(sage))
