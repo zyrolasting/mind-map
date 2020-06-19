a740cbb7ba06c6daf37d6748e938030b2d21d509
Sage Gerard <sage@sagegerard.com>
2020-06-19 15:39:53 -0400

    Add examples

---
+++ examples/tcgs.rkt
@@ -0,0 +1,20 @@
+#lang mind-map
+
+Deck-building trading card games
+  Expensive if you care about tournaments and copyright laws. Free if you don't.
+  If your child is a fan, find them a new outlet for competition NOW.
+
+  Magic: The Gathering (Win conditions)
+    Reduce your opponent's life to zero
+    Your opponent's deck is out of cards when they need to draw.
+    Meet a win condition for a particular card.
+
+  Yu-Gi-Oh
+    MTG for people who think anime and Japanese culture are the same thing
+    fRiEnDsHip cOnQuers AlL
+    Win conditions similar to MTG
+
+  Hearthstone
+    Everything is digital except the money you pay
+    Pretty colors
+    Noisy
